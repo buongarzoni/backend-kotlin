12edaad6dde16922858cd30544ec02f4a7b20413
cbuongarzoni <christian.buongarzoni@mercadolibre.com>
2022-05-01 17:10:40 -0300

    refactor

--- src/main/kotlin/com/codely/demo/fizz_buzz/FizzBuzzGenerator.kt
+++ src/main/kotlin/com/codely/demo/fizz_buzz/FizzBuzzGenerator.kt
@@ -1,10 +1,21 @@
 package com.codely.demo.fizz_buzz
 
+private const val MULTIPLES_OF_THREE_MESSAGE = "Fizz"
+private const val MULTIPLES_OF_FIVE_MESSAGE = "Buzz"
+private const val MULTIPLES_OF_FIFTEEN_MESSAGE = "FizzBuzz"
+
 class FizzBuzzGenerator {
-    fun generate(value: Int) : String {
-        if (value % 15 == 0) return "FizzBuzz"
-        if (value % 3 == 0) return "Fizz"
-        if (value % 5 == 0) return "Buzz"
+
+    fun generate(value: Int): String {
+        return when {
+            (value % 15 == 0) -> MULTIPLES_OF_FIFTEEN_MESSAGE
+            (value % 3 == 0) -> MULTIPLES_OF_THREE_MESSAGE
+            (value % 5 == 0) -> MULTIPLES_OF_FIVE_MESSAGE
+            else -> defaultMessage(value)
+        }
+    }
+
+    private fun defaultMessage(value: Int): String {
         return value.toString()
     }
 }
